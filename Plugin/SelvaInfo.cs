dadd10005d68e63f154cd8346ecc6104b66dbab9
Felix Brunold <48569186+TheVessen@users.noreply.github.com>
2025-12-08 15:47:16 +0100

    feat: Add favicon and web app manifest for Selva plugin

--- Plugin/SelvaInfo.cs
+++ Plugin/SelvaInfo.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Drawing;
 using Grasshopper.Kernel;
+using Selva.Properties;
 
 namespace Selva;
 
@@ -9,7 +10,7 @@ public class SelvaInfo : GH_AssemblyInfo
   public override string Name => "Selva";
 
   //Return a 24x24 pixel bitmap to represent this GHA library.
-  public override Bitmap Icon => null;
+  public override Bitmap Icon => Resources.IconPlugin;
 
   //Return a short string describing the purpose of this GHA library.
   public override string Description => "";
@@ -17,10 +18,10 @@ public class SelvaInfo : GH_AssemblyInfo
   public override Guid Id => new("69ef43c6-0bc8-4b49-8e4d-a27f732cc10b");
 
   //Return a string identifying you or your company.
-  public override string AuthorName => "";
+  public override string AuthorName => "Felix Brunold, VektorNode";
 
   //Return a string representing your preferred contact details.
-  public override string AuthorContact => "";
+  public override string AuthorContact => "felix@vektornode.com";
 
   //Return a string representing the version.  This returns the same version as the assembly.
   public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
