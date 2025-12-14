b276a3fd306105754c69c5aff6c5cd43de89ef78
Felix Brunold <48569186+TheVessen@users.noreply.github.com>
2025-12-14 17:24:18 +0100

    style: Adjust indentation and formatting in Logger class for improved readability

--- Plugin/Core/Helpers/Logger.cs
+++ Plugin/Core/Helpers/Logger.cs
@@ -3,28 +3,28 @@ using Rhino;
 
 namespace Selva.Core.Helpers
 {
-    public static class Logger
+  public static class Logger
+  {
+    public static void Log(string message)
     {
-        public static void Log(string message)
-        {
 #if DEBUG
-            RhinoApp.WriteLine($"[Selva] {message}");
+      RhinoApp.WriteLine($"[Selva] {message}");
 #endif
-        }
-
-        public static void Error(string message, Exception ex = null)
-        {
-            RhinoApp.WriteLine($"[Selva Error] {message}");
-            if (ex != null)
-            {
-                RhinoApp.WriteLine($"[Selva Error Details] {ex.Message}");
-                RhinoApp.WriteLine($"[Selva Error Stack] {ex.StackTrace}");
-            }
-        }
-        
-        public static void Warn(string message)
-        {
-             RhinoApp.WriteLine($"[Selva Warning] {message}");
-        }
     }
+
+    public static void Error(string message, Exception ex = null)
+    {
+      RhinoApp.WriteLine($"[Selva Error] {message}");
+      if (ex != null)
+      {
+        RhinoApp.WriteLine($"[Selva Error Details] {ex.Message}");
+        RhinoApp.WriteLine($"[Selva Error Stack] {ex.StackTrace}");
+      }
+    }
+
+    public static void Warn(string message)
+    {
+      RhinoApp.WriteLine($"[Selva Warning] {message}");
+    }
+  }
 }
