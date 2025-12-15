4637875b3f87b72393392b2caa1730f86390ea9a
Felix Brunold <48569186+TheVessen@users.noreply.github.com>
2025-12-15 12:47:47 +0100

    refactor: Clean up formatting and improve readability in test files

--- Plugin/Selva.Tests/SchemaMigratorTests.cs
+++ Plugin/Selva.Tests/SchemaMigratorTests.cs
@@ -1,64 +1,60 @@
-using System;
-using System.Collections.Generic;
-using Xunit;
 using Selva.Core.Models;
 using Selva.Core.Services;
 
-namespace Selva.Tests
+namespace Selva.Tests;
+
+public class SchemaMigratorTests
 {
-  public class SchemaMigratorTests
+  [Fact]
+  public void MigrateWithTracking_NullSchema_ThrowsArgumentNullException()
   {
-    [Fact]
-    public void MigrateWithTracking_NullSchema_ThrowsArgumentNullException()
+    Assert.Throws<ArgumentNullException>(() =>
+      SchemaMigrator.MigrateWithTracking(null, new Version(1, 0, 0)));
+  }
+
+  [Fact]
+  public void MigrateWithTracking_LegacySchema_SetsVersionTo100()
+  {
+    var schema = new UISchema
     {
-      Assert.Throws<ArgumentNullException>(() =>
-          SchemaMigrator.MigrateWithTracking(null, new Version(1, 0, 0)));
-    }
+      Id = "legacy-schema",
+      SchemaVersion = null // Legacy
+    };
 
-    [Fact]
-    public void MigrateWithTracking_LegacySchema_SetsVersionTo100()
+    var (migratedSchema, changes) = SchemaMigrator.MigrateWithTracking(schema, new Version(1, 0, 0));
+
+    Assert.Equal("1.0.0", migratedSchema.SchemaVersion);
+    Assert.Contains(changes, c => c.Contains("legacy schema detected"));
+  }
+
+  [Fact]
+  public void MigrateWithTracking_IncompatiblePluginVersion_ThrowsException()
+  {
+    var schema = new UISchema
     {
-      var schema = new UISchema
-      {
-        Id = "legacy-schema",
-        SchemaVersion = null // Legacy
-      };
+      Id = "future-schema",
+      SchemaVersion = "1.0.0",
+      MinPluginVersion = "2.0.0"
+    };
 
-      var (migratedSchema, changes) = SchemaMigrator.MigrateWithTracking(schema, new Version(1, 0, 0));
+    var currentVersion = new Version(1, 0, 0);
 
-      Assert.Equal("1.0.0", migratedSchema.SchemaVersion);
-      Assert.Contains(changes, c => c.Contains("legacy schema detected"));
-    }
+    Assert.Throws<IncompatibleSchemaException>(() =>
+      SchemaMigrator.MigrateWithTracking(schema, currentVersion));
+  }
 
-    [Fact]
-    public void MigrateWithTracking_IncompatiblePluginVersion_ThrowsException()
+  [Fact]
+  public void MigrateWithTracking_CurrentVersion_NoChanges()
+  {
+    var schema = new UISchema
     {
-      var schema = new UISchema
-      {
-        Id = "future-schema",
-        SchemaVersion = "1.0.0",
-        MinPluginVersion = "2.0.0"
-      };
+      Id = "current-schema",
+      SchemaVersion = SchemaMigrator.CURRENT_SCHEMA_VERSION.ToString()
+    };
 
-      var currentVersion = new Version(1, 0, 0);
+    var (migratedSchema, changes) = SchemaMigrator.MigrateWithTracking(schema, new Version(1, 0, 0));
 
-      Assert.Throws<IncompatibleSchemaException>(() =>
-          SchemaMigrator.MigrateWithTracking(schema, currentVersion));
-    }
-
-    [Fact]
-    public void MigrateWithTracking_CurrentVersion_NoChanges()
-    {
-      var schema = new UISchema
-      {
-        Id = "current-schema",
-        SchemaVersion = SchemaMigrator.CURRENT_SCHEMA_VERSION.ToString()
-      };
-
-      var (migratedSchema, changes) = SchemaMigrator.MigrateWithTracking(schema, new Version(1, 0, 0));
-
-      Assert.Same(schema, migratedSchema);
-      Assert.Empty(changes);
-    }
+    Assert.Same(schema, migratedSchema);
+    Assert.Empty(changes);
   }
 }
