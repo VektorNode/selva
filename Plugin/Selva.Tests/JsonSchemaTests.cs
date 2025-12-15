9d07657825ddbca076e00eb7a360ffed3f4cbadc
Felix Brunold <48569186+TheVessen@users.noreply.github.com>
2025-12-15 12:45:24 +0100

    feat: Add JsonSchemaTests and valid_schema.json for schema validation testing

---
+++ Plugin/Selva.Tests/JsonSchemaTests.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using Xunit;
+using Newtonsoft.Json;
+using Selva.Core.Models;
+using Selva.Core.Services;
+
+namespace Selva.Tests
+{
+  public class JsonSchemaTests
+  {
+    private readonly SchemaValidator _validator;
+
+    public JsonSchemaTests()
+    {
+      _validator = new SchemaValidator();
+    }
+
+    [Theory]
+    [InlineData("valid_schema.json")]
+    public void Validate_JsonSchema_ReturnsSuccess(string fileName)
+    {
+      var filePath = Path.Combine("TestFiles", fileName);
+      var json = File.ReadAllText(filePath);
+      var schema = JsonConvert.DeserializeObject<UISchema>(json);
+
+      var result = _validator.Validate(schema);
+
+      Assert.True(result.IsValid);
+    }
+  }
+}
