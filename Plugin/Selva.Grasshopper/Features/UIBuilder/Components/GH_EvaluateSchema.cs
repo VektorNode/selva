4d9656073011452cbfe36729c7166e2cbc96e6f6
Felix Brunold <48569186+TheVessen@users.noreply.github.com>
2025-12-18 14:25:08 +0100

    feat: add GH_EvaluateSchema component with basic structure and placeholders

---
+++ Plugin/Selva.Grasshopper/Features/UIBuilder/Components/GH_EvaluateSchema.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Selva.Grasshopper.Features.UIBuilder.Components;
+
+public class GH_EvaluateSchema : GH_Component
+{
+
+  public override Guid ComponentGuid
+  {
+    get { return new Guid("E7611CB2-9BAE-4A88-B47B-A94135394FA3"); }
+  }
+
+  protected override Bitmap Icon => null;
+
+  public GH_EvaluateSchema()
+    : base("GH_EvaluateSchema", "Nickname",
+      "Description",
+      "Selva", "Subcategory")
+  {
+  }
+
+
+  protected override void RegisterInputParams(GH_InputParamManager pManager)
+  {
+  }
+
+
+  protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+  {
+  }
+
+  protected override void SolveInstance(IGH_DataAccess DA)
+  {
+  }
+
+}
+
