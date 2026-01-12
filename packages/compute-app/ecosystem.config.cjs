5c16a537ca2a420a16f6661be12c70dc4c751064
Felix Brunold <48569186+TheVessen@users.noreply.github.com>
2026-01-12 08:29:01 +0100

    feat: change ecosystem to cjs instead of js

---
+++ packages/compute-app/ecosystem.config.cjs
@@ -0,0 +1,15 @@
+// This file is used by PM2 to manage the compute app process when deployed with NODE
+module.exports = {
+	apps: [
+		{
+			name: 'selva-compute',
+			script: './build/index.js',
+			env_file: '.env',
+			instances: 1,
+			exec_mode: 'fork',
+			autorestart: true,
+			watch: false,
+			max_memory_restart: '1G'
+		}
+	]
+};
