32a93cd885a7656af4b2d6a0146c34edf4430f1c
Felix Brunold <48569186+TheVessen@users.noreply.github.com>
2026-05-11 16:33:59 +0200

    feat: enhance resizable handle appearance and functionality with improved styles and transitions

--- packages/ui/src/lib/components/primitives/resizable/resizable-handle.svelte
+++ packages/ui/src/lib/components/primitives/resizable/resizable-handle.svelte
@@ -16,12 +16,14 @@
 	bind:ref
 	data-slot="resizable-handle"
 	class={cn(
-		'cn-resizable-handle after:inset-y-0 after:w-1 data-[direction=vertical]:after:left-0 data-[direction=vertical]:after:h-1 data-[direction=vertical]:after:translate-x-0 relative flex w-px items-center justify-center bg-border after:absolute after:left-1/2 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:outline-hidden data-[direction=vertical]:h-px data-[direction=vertical]:w-full data-[direction=vertical]:after:w-full data-[direction=vertical]:after:-translate-y-1/2 [&[data-direction=vertical]>div]:rotate-90',
+		'cn-resizable-handle group/handle after:inset-y-0 after:w-1 data-[direction=vertical]:after:left-0 data-[direction=vertical]:after:h-1 data-[direction=vertical]:after:translate-x-0 relative flex w-px items-center justify-center bg-border after:absolute after:left-1/2 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:outline-hidden data-[direction=vertical]:h-px data-[direction=vertical]:w-full data-[direction=vertical]:after:w-full data-[direction=vertical]:after:-translate-y-1/2 [&[data-direction=vertical]>div]:rotate-90',
 		className
 	)}
 	{...restProps}
 >
 	{#if withHandle}
-		<div class="h-6 w-1 z-10 flex shrink-0 rounded-lg bg-border"></div>
+		<div
+			class="h-10 w-1 z-10 flex shrink-0 rounded-lg bg-border transition-[background-color,transform] duration-150 group-hover/handle:bg-foreground/40 group-hover/handle:scale-y-110 group-active/handle:bg-foreground/60"
+		></div>
 	{/if}
 </ResizablePrimitive.PaneResizer>
