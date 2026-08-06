777cb775dc1c1bb0bf9d2480206c2ea3fa6378a3
TheVessen <xilef1997@hotmail.com>
2026-08-06 20:40:26 +0200

    feat: add node engine detection tests for CLI functionality

--- packages/cli/bin/create.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { runCreate } from '../src/commands/create.js';
-
-runCreate(process.argv.slice(2)).catch((err) => {
-	console.error(err instanceof Error ? err.message : err);
-	process.exit(1);
-});
